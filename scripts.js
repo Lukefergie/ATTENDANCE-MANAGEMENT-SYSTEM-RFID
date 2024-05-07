d10e38a32304fb222bff3d91ee0444945f5f60db
IvyTrink <97182828+Lukefergie@users.noreply.github.com>
2024-05-07 19:51:15 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Your response has been recorded. We will get back to you soon!');
+});
+
+document.getElementById('newsletter-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for reaching us your problem will resolve shortly!');
+});
